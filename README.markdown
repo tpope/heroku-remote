3f72d2dc756919230b2caccc60d77317efedb9c1
Tim Pope <code@tpope.net>
2013-03-01 15:53:35 -0500

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,80 @@
+# Heroku Git remote commands
+
+By default, the `heroku` command operates on the application referred to by
+the `heroku` Git remote.  This is great for simple apps, but starts to break
+down when you need multiple Heroku apps to serve up different environments
+(staging, production, and so on).  You end up having to set up a bunch of
+remotes by hand, and then your reward is the eternal obligation to explicitly
+pass in the remote: `heroku console -r staging` and the like.  My solution to
+this problem is twofold:
+
+1.  Create [binstubs][] for each Heroku app.  For example, in `bin/staging`:
+
+        #!/bin/sh
+        HEROKU_APP=myapp-staging exec heroku "$@"
+
+    Now you can do `staging logs`, `staging info`, and any other Heroku
+    command without any `--app` or `--remote` insanity.
+
+2.  Use this plugin, which encapsulates common operations on the Git remote.
+
+## Installation
+
+    $ heroku plugins:install https://github.com/tpope/heroku-remote.git
+
+## Commands
+
+See `heroku help remote` for the complete list.  If you've gone the
+[binstubs][] route, you'll want to change `heroku` to the name of your
+binstub.
+
+### heroku push
+
+Push to the appropriate Git remote or URL.  You can give any valid `git push`
+argument, including the very useful `--force`.  The default is to push
+`HEAD` to `master`, effectively deploying the current commit.
+
+If the first argument does not contain a colon, `:master` is appended.  Use
+`my-branch:my-branch` if you really want to push something other than a
+deployment.
+
+This is aliased as `deploy`, so the instinctive `heroku deploy` works.
+
+### heroku fetch
+
+Fetch the Git remote associated with the given application.  If no Git remote
+exists, fetch the repository URL directly, meaning the results will only be
+available in `FETCH_HEAD`.
+
+### heroku log
+
+Show a Git log for the currently deployed commit.  Does a `heroku fetch` if
+necessary.
+
+    heroku log --pretty=oneline
+
+You can also give releases and they will be translated into commit SHAs.
+
+    heroku log v123..v124
+
+Not to be confused with `heroku logs`.
+
+### heroku checkout
+
+Check out the currently deployed release commit.  Does a `heroku fetch` if
+necessary.  Accepts an optional release to check out instead.
+
+    heroku checkout -b still-working v122
+
+### heroku remote:add
+
+Adds the application as a Git remote named after itself.  This is useful if
+you need to break out of the box offered by the above commands.
+
+    heroku remote:add --app myapp-staging
+
+Normally you would use this with [binstubs][]:
+
+    staging remote:add
+
+[binstubs]: https://github.com/sstephenson/rbenv/wiki/Understanding-binstubs
---
+++ lib/heroku/command/remote.rb
@@ -0,0 +1,167 @@
+# work with the remote repository
+class Heroku::Command::Remote < Heroku::Command::Base
+
+  # remote
+  #
+  # show the Git remote name or Git URL for the app
+  def index
+    display(remote_name || git_url)
+  end
+
+  # remote:url
+  #
+  # show the Git URL for the app
+  def url
+    display(git_url)
+  end
+
+  # remote:name
+  #
+  # show the name of the Git remote for the app
+  def name
+    if remote_name
+      display(remote_name)
+    else
+      error("No remote for application #{app}")
+    end
+  end
+
+  # remote:add [NAME]
+  #
+  # add a remote for the app
+  #
+  # If NAME is not specified, it defaults to the same name as the app itself.
+  def add
+    create_git_remote(shift_argument || app, git_url)
+  end
+
+  # push [REFSPEC ...]
+  #
+  # git push the given REFSPEC to the app remote
+  #
+  # If REFSPEC is not specified, it defaults to HEAD:master.  If it is
+  # specified but does not contain a colon, :master will be appended.
+  #
+  # -f, --force   # force push
+  # -n, --dry-run # don't actually send the updates
+  def push
+    refspec = shift_argument || 'HEAD'
+    refspec << ':master' unless refspec.include?(':') || refspec =~ /^-/
+    args.unshift('--force') if options[:force]
+    args.unshift('--dry-run') if options[:dry_run]
+    system('git', 'push', remote_name || git_url, refspec, *args)
+  end
+  alias_command "push", "remote:push"
+  # If someone has a more elaborate "deploy" command, let it take precedence.
+  unless Heroku::Command.command_aliases['deploy']
+    alias_command "deploy", "remote:push"
+  end
+
+  # fetch [REFSPEC ...]
+  #
+  # git fetch from the app Git remote
+  #
+  # If a Git remote exists for the application, fetch that remote.  Otherwise,
+  # fetch the underlying repository URL, leaving the results only in
+  # FETCH_HEAD.
+  def fetch
+    system('git', 'fetch', remote_name || git_url, *args)
+  end
+  alias_command 'fetch', 'remote:fetch'
+
+  # log [...]
+  #
+  # invoke git log on a release commit
+  #
+  # Translates any arguments that look like releases (v followed by digits) to
+  # SHAs and invoke git log.  If no releases appear in the argument list, the
+  # latest release is added.
+  #
+  # Not to be confused with the server logs command "logs".
+  #
+  #Examples:
+  #
+  # $ heroku log -p
+  # $ heroku log v101
+  # $ heroku log v102..v103
+  def log
+    found = false
+    arguments = args.map do |arg|
+      case arg
+      when /^-/, '..', '...'
+        arg
+      when /^(.*?)(\.{2,3})(.*)$/
+        found = true
+        "#{fetch_release_commit($1)}#$2#{fetch_release_commit($3)}"
+      when /^(v\d+)$/
+        found = true
+        "#{fetch_release_commit($2)}"
+      else
+        arg
+      end
+    end
+    arguments.unshift(fetch_release_commit) unless found
+    system('git', 'log', *arguments)
+  end
+  alias_command 'log', 'remote:log'
+
+  # checkout [RELEASE] [...]
+  #
+  # invoke git checkout on a release commit
+  #
+  # Defaults to the latest release.
+  #
+  #Examples:
+  #
+  # $ heroku checkout
+  # $ heroku checkout -b not_broken v123
+  def checkout
+    release = nil
+    args.delete_if do |arg|
+      if !release && arg =~ /^v\d+$/
+        release = arg
+      end
+    end
+    system('git', 'checkout', fetch_release_commit(release), *args)
+  end
+  alias_command 'checkout', 'remote:checkout'
+
+  private
+
+  def remote_name
+    @remote_name ||= git_remotes.invert.fetch(app, git_url)
+  end
+
+  def git_url
+    @git_url ||= api.get_app(app).body['git_url']
+  end
+
+  def fetch_commit(sha)
+    if git("rev-parse --quiet --verify #{sha}").empty?
+      unless @fetched
+        git("fetch #{remote_name || git_url}")
+        @fetched = true
+      end
+    end
+    sha
+  end
+
+  def get_release(release = nil)
+    if release.to_s.empty?
+      api.get_releases(app).body.last
+    else
+      api.get_release(app, release).body
+    end
+  end
+
+
+  def fetch_release_commit(release = nil)
+    if release.to_s.empty? || release =~ /^v\d+$/
+      fetch_commit(get_release(release)['commit'])
+    else
+      raise release
+      release
+    end
+  end
+
+end
